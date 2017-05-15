0ac83fd9c185ab3684aaab1acffa62324dfbbf96
admin <dlagazo@gmail.com>
2017-05-16 07:33:44 +0800

    master

---
+++ Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acc_hris.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
